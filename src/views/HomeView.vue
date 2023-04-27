d47604f67410aaddd1d57b3218f26ba6ac4e70f3
GXic07 <1512360075@qq.com>
2023-04-27 11:58:22 +0800

    modified:   src/views/HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="我是傻逼 你也是 大家都是 "/>
+    <HelloWorld msg="我是傻逼 你也是 大家都是 你们全部人都是傻逼 "/>
   </div>
 </template>
 
